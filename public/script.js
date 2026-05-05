f4bad28042999b0ec6f229b682d3f5efc1cee351
Tony Omer <tonyomer551@gmail.com>
2026-05-05 17:32:19 +0400

    changing colors

--- public/script.js
+++ public/script.js
@@ -6,11 +6,11 @@ axios.get("/api/weather")
     data.forEach((weather, index) => {
       const p = boxes[index].querySelectorAll("p");
 
-      p[0].innerText = `Temperature: ${weather.temperature} °C`;
-      p[1].innerText = `Condition: ${weather.condition}`;
-      p[2].innerText = `Humidity: ${weather.humidity}%`;
-      p[3].innerText = `Wind Speed: ${weather.windSpeed} m/s`;
-      p[4].innerText = `Feels Like: ${weather.feelsLike} °C`;
+      p[0].innerText = `Temperature 🌡: ${weather.temperature} °C`;
+      p[1].innerText = `Condition  ☁ : ${weather.condition}`;
+      p[2].innerText = `Humidity 💧 : ${weather.humidity}%`;
+      p[3].innerText = `Wind Speed 💨 : ${weather.windSpeed} m/s`;
+      p[4].innerText = `Feels Like 🤒 : ${weather.feelsLike} °C`;
     });
   })
   .catch(err => {
